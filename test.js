c3e0d3d1858f9a230c451b8cf820c8b7268acf17
ahikmatf <asephikmatf@gmail.com>
2017-12-29 09:41:11 +0700

    Initial Commit

---
+++ test.js
@@ -0,0 +1,17 @@
+function abc() {
+	console.log('aku abc')
+	alert()
+}
+
+function cde() {
+	console.log('aku cde')
+	function abc() {
+		console.log('aku abc')
+	}
+}
+
+function efg() {
+	console.log('aku efg')
+	alert()
+}
+
